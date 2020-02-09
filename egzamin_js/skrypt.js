e0228e7d77c0ac79d55ff4b1ae224769ab460330
oobanana <39858563+oobanana@users.noreply.github.com>
2020-02-09 11:21:32 +0100

    Add files via upload

---
+++ egzamin_js/skrypt.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', function(){
+    var bopen=document.getElementById('button-mapa');
+
+    bopen.addEventListener('click', function(){
+        window.open('https://www.google.com/maps/place/Politechnika+Bia%C5%82ostocka/@53.1169458,23.1436831,17z/data=!3m1!4b1!4m5!3m4!1s0x471ffbfb9415aee5:0x1f6449ccc6c966f7!8m2!3d53.1169458!4d23.1458718');
+    })
+})
